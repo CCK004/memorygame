14639977a5121b3df62f0dabb65fe417cbc46d6d
CCK004 <148968721+CCK004@users.noreply.github.com>
2023-11-12 18:03:41 +0800

    tt

--- main.js
+++ main.js
@@ -8,16 +8,26 @@ let revealedCount = 0;
 let activeTile = null;
 let awaitingEndOfMove = false;
 
+function buildTile(color){
+    const element = document.createElement("div");
+
+    element.classList.add("tile");
+    element.setAttribute("data-color", color);
+
+    return element;
+
+}
 
 // Build up tiles
 for (let i = 0; i < tileCount; i++){
     const randomIndex = Math.floor(Math.random() * colorsPicklist.length);
     const color = colorsPicklist[randomIndex];
+    const tile = buildTile(color);
 
     colorsPicklist.splice(randomIndex, 1);
 
     console.log(color);
+    document.body.appendChild(Tile);
 }
-
 
 console.log(colorsPicklist);
