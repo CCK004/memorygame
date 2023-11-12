00daac1383a44035e552b805b15231433fa5e7db
CCK004 <148968721+CCK004@users.noreply.github.com>
2023-11-12 17:56:34 +0800

    tt

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const tilesContainer = document.querySelector(".tiles");
 const colors = ["aqua", "aquamarine", "crimson", "blue", "dodgerblue", "gold", "greenyellow", "teal"];
+const colorsPicklist = [...colors, ...colors];
+const tileCount = colorsPicklist.length;
+
+//Game state
+let revealedCount = 0;
+let activeTile = null;
+
+
+
+console.log(colorsPicklist);
