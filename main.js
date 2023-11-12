40348f70f7029e154c4ed741b168b14ba3e6fa03
CCK004 <148968721+CCK004@users.noreply.github.com>
2023-11-12 18:00:42 +0800

    tt

--- main.js
+++ main.js
@@ -3,10 +3,21 @@ const colors = ["aqua", "aquamarine", "crimson", "blue", "dodgerblue", "gold", "
 const colorsPicklist = [...colors, ...colors];
 const tileCount = colorsPicklist.length;
 
-//Game state
+// Game state
 let revealedCount = 0;
 let activeTile = null;
+let awaitingEndOfMove = false;
 
+
+// Build up tiles
+for (let i = 0; i < tileCount; i++){
+    const randomIndex = Math.floor(Math.random() * colorsPicklist.length);
+    const color = colorsPicklist[randomIndex];
+
+    colorsPicklist.splice(randomIndex, 1);
+
+    console.log(color);
+}
 
 
 console.log(colorsPicklist);
